1fd53dff340bc7d46618290a3944b8ba56654087
Abhishek Singh Chaudhary <79990244+AB1234567-PROG@users.noreply.github.com>
2024-03-03 20:51:39 +0530

    Time: 1 ms (64.24%), Space: 42.6 MB (52.67%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         
-        List<List<Integer>> resultList = new ArrayList<>();
-
-    // Start backtracking from the beginning
-    backtrack(resultList, new ArrayList<>(), nums, 0);
-    return resultList;
-  }
-
-  private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet,
-                         int[] nums, int start) {
-    // Add the set to result set
-    resultSets.add(new ArrayList<>(tempSet));
-    for (int i = start; i < nums.length; i++) {
-
-      // Case of including the number
-      tempSet.add(nums[i]);
-
-      // Backtrack the new subset
-      backtrack(resultSets, tempSet, nums, i + 1);
-
-      // Case of not-including the number
-      tempSet.remove(tempSet.size() - 1);
-    }  
-
+        List<List<Integer>> res = new ArrayList<>();
         
+       return generate(res, new ArrayList<>(), nums, 0);
+       
+     }
+    
+    public List<List<Integer>> generate(List<List<Integer>> res, List<Integer> tempSet, int[] nums, int start)
+    {
+        
+        res.add(new ArrayList<>(tempSet));
+        for(int i = start; i<nums.length; i++)
+        {
+            tempSet.add(nums[i]);
+            
+            generate(res, tempSet, nums, i+1);
+            
+            tempSet.remove(tempSet.size()-1);
+        }
+        return res;
     }
+    
 }
