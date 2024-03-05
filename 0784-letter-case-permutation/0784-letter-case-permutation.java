5139ba7e84272aaa233baa02a2e80917e9d7753f
Abhishek Singh Chaudhary <79990244+AB1234567-PROG@users.noreply.github.com>
2024-03-05 12:08:51 +0530

    Time: 7 ms (52.67%), Space: 45.7 MB (31.28%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        
+        List<String> res = new ArrayList<>();
+        
+        String s1 = "";
+        //+=s.charAt(0);
+        String s2 = s.substring(0);
+        
+        return generate(res,s1,s2);
+        
+    }
+      List<String> generate(List<String> res, String s1, String s2)
+      {
+          if(s2.length()==0)
+          {
+              res.add(s1);
+              return res;
+          }
+          
+          char c=s2.charAt(0);
+          char c1=s2.charAt(0);
+          
+          if(c>='a' && c<='z')
+             c=Character.toUpperCase(c);
+          else if(c>='A' && c<='Z')
+              c=Character.toLowerCase(c);
+          
+          generate(res, s1+c1,s2.substring(1));
+          if(!(c>='0' && c<='9'))
+          generate(res, s1+c,s2.substring(1));
+          
+          return res;
+              
+          
+              
+              
+          
+          
+      }
+       
+}
