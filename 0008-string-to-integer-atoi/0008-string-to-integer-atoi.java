cc954b55db2c86569e2eabc5755e5c695ed84f31
Abhishek Singh Chaudhary <79990244+AB1234567-PROG@users.noreply.github.com>
2024-03-29 23:40:17 +0530

    Time: 2 ms (38.49%), Space: 42.4 MB (49.05%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,31 +1,44 @@
 class Solution {
     public int myAtoi(String s) {
         
-        if(s==null) return 0;
-
-        s=s.trim();
-        
-        if(s.length()==0) return 0;
-        
-        int sign = +1;
-        long ans = 0;
-        if(s.charAt(0) == '-') sign = -1;
-        
-        int MAX = Integer.MAX_VALUE, MIN = Integer.MIN_VALUE;
-		
-		// initiate the starting pointer
-        int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;
-		
-        while(i < s.length()) {
-            if(s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i))) break;
-            ans = ans * 10 + s.charAt(i)-'0';
-            // check the conditions
-            if(sign == -1 && -1*ans < MIN) return MIN;
-            if(sign == 1 && ans > MAX) return MAX;
-            
-            i++;
-        }
-        
-        return (int)(sign*ans);
+        char sign = ' ';
+        double sum=0;
+        int count=0;
+        boolean flag = false;
+       for(int i=0; i<s.length();i++)
+       {
+           if(count>1)
+           return (int)sum;
+           else if(s.charAt(i)>='0' && s.charAt(i)<='9'&& flag==false)
+              sum=sum*10+s.charAt(i)-'0';
+           else if(i!=s.length()-1 && (s.charAt(i)=='+' || s.charAt(i)=='-'))
+             {  
+                if(i!=0 && s.charAt(i-1)>='0' && s.charAt(i-1)<='9')
+                flag=true;
+                else
+                {
+                sign=s.charAt(i);
+                count++;
+                }
+             }
+                 
+           else if(s.charAt(i)==' ')
+           {
+            if(i!=0 && ((s.charAt(i-1)>='0' && s.charAt(i-1)<='9')|| s.charAt(i-1)=='+' || s.charAt(i-1)=='-'))
+            flag=true;
+            else
+            continue;
+           }
+         
+           else
+           flag=true; 
+       }
+        if(sign=='-')
+            sum=0-sum;
+        if(sum>=Math.pow(2,31))
+            sum=Math.pow(2,31)-1;
+        else if(sum<Math.pow(-2,31))
+            sum=Math.pow(-2,31);
+        return (int)sum;
     }
 }
